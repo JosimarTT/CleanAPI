947b79a50550b4bc08048f86d2c38fca5c74ecf1
Josimar Tantahuilca Torres <sagadeath@hotmail.com>
2021-07-07 16:01:46 -0500

    First endpoint working

---
+++ CleanAPI.API/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using CleanAPI.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok(posts);
+        }
+    }
+}
---
+++ CleanAPI.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanAPI.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var post = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            });
+            return post;
+        }
+    }
+}
